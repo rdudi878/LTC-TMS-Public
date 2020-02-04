e446e04ae2c78b7d22ab2095675ea2a7d9f5d9d6
Sam <rdudi878@live.kutztown.edu>
2020-02-04 16:46:19 -0500

    finally got the vital status info from the db to print in the console

--- CSC355-Browser-Team/Frontend/DVSR.js
+++ CSC355-Browser-Team/Frontend/DVSR.js
@@ -69,46 +69,15 @@ fbPAT.once("value")
       var selectedPatient = $('#selectPAT').val()
       var selectedDate = $('#bday').val()
       selectedPatient = getSecondPart(selectedPatient) // Split the string to ignore the name portion of the dropdown selection
-      var patientRef = firebase.database().ref("Activities");
-      var patientChild = patientRef.child(selectedPatient);
-      console.log(patientChild.child("330001").child("vital_status"));
-      patientChild.on('value', gotPatientInfo, patientInfoError);
+      var rootRef = firebase.database().ref();
+// Note: will need to get the CNA ID some other way.
+      var statusRef = rootRef.child("Activities/"+selectedPatient+"/"+selectedDate+"/330001/vital_status/");
+      statusRef.once("value", function(snapshot) {
+        snapshot.forEach(function(child) {
+          console.log(child.key+": "+child.val());
+        });
+      });
 
-      function gotPatientInfo(data) {
-        var dates = data.val();
-        var keys = Object.keys(dates);
-        var dateRef = patientChild.child(selectedDate);
-        var cnoRef = dateRef.child("330001");
-        var vitalStatRef = cnoRef.child("vital_status");
-        console.log("dateRef: ");
-        console.log(dateRef);
-        console.log("cnoRef: ");
-        console.log(cnoRef);
-        console.log("vitalStatRef");
-        console.log(vitalStatRef);
-        // console.log(keys);
-//         for (var i=0; i<keys.length; i++) {
-//           // console.log(keys[i]);
-//           if (keys[i]==selectedDate) {
-//             console.log("MATCH");
-//             // console.log("keys[i]:");
-//             // console.log(keys[i]);
-//             // console.log("selectedDate:");
-//             // console.log(selectedDate);
-// // Now do something here since a match was found w/ patient info
-//
-//           } else {
-//             console.log("no match found");
-//           }
-//         }
-// print the patient info in a legible format w/ .val
-// instead of some goofy-lookin' javasript object
-        // console.log(data.val());
-      }
-      function patientInfoError(err) {
-        console.log("patient info error");
-        console.log(err);
-      }
       function getSecondPart(str) {
         return str.split('- ')[1];
       }
