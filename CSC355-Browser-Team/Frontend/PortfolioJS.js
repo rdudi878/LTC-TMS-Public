336e48e989327d2f2e9cc70038b11d27d1e0e726
unknown <rdudi878@live.kutztown.edu>
2020-02-21 17:04:24 -0500

    sort E-portfolio by any column

--- CSC355-Browser-Team/Frontend/PortfolioJS.js
+++ CSC355-Browser-Team/Frontend/PortfolioJS.js
@@ -104,7 +104,7 @@ function handleuploadfileSubmit(e) {
   var storageRef3 = firebase.storage().ref('Staff/'+StaffID+'/'+file3.name) //cv
   var storageRef4 = firebase.storage().ref('Staff/'+StaffID+'/'+file4.name) //license
   //var storageRef = firebase.storage().ref('Sponsor/'+name+".png")
-  
+
   var uploadtask = storageRef1.put(file1);
 
   uploadtask.on('state_changed',
@@ -149,7 +149,7 @@ function handleuploadfileSubmit(e) {
           cvFilename: file3.name,
           licenseFilename: file4.name
         };
-        
+
         updates[ staffPosition +'/'+StaffID +'/Portfolio/'] = postData;
 
         firebase.database().ref().update(updates);
@@ -468,7 +468,7 @@ function viewP(){
 
             //TODO:image input and resize
             document.getElementById('imgS').src = photo;
-            
+
             document.getElementById('ssPassword').innerHTML = "*****";
             document.getElementById('sERelationship').innerHTML = ERelationship;
             document.getElementById('sEContact').innerHTML = EContact;
@@ -1233,3 +1233,18 @@ function checkCNOID(){
     }
   });
 }
+
+$('th').click(function(){
+    var table = $(this).parents('table').eq(0)
+    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+    this.asc = !this.asc
+    if (!this.asc){rows = rows.reverse()}
+    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+})
+function comparer(index) {
+    return function(a, b) {
+        var valA = getCellValue(a, index), valB = getCellValue(b, index)
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+    }
+}
+function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
